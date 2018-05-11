3b0da9f56423adf282edbcd5f41d74aaa38af157
Semyon Arkhipov <sem8252@yandex.ru>
2018-05-11 21:09:21 +0300

    Modificated BD

--- ASUSP/DataConvert.cs
+++ ASUSP/DataConvert.cs
@@ -32,7 +32,8 @@ namespace ASUSP
 
         public static void WriteToCSV(List<Transaction> transactions)
         {
-            var csv = new StringBuilder();
+            //var csv = new StringBuilder();
+            List<string> lines = new List<string>();
             foreach (Transaction current in transactions)
             {
                 var number = current.ID.ToString();
@@ -47,14 +48,15 @@ namespace ASUSP
                 var status = current.status.ToString();
                 var newLine = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
                 number, start, end, type, code, country, city, firm, cell, status);
-                csv.Append(newLine);
+                //csv.Append(newLine);
+                lines.Add(newLine);
             }
-            File.WriteAllText(path, csv.ToString());
+            File.WriteAllLines(path, lines);
         }
 
         public static List<Transaction> ReadFromCSV()
         {
-            var lines = File.ReadAllText(path);
+            var lines = File.ReadAllLines(path);
             var list = new List<Transaction>();
             foreach (var line in lines)
             {
@@ -73,8 +75,8 @@ namespace ASUSP
                 list.Add(transaction);
             }
             
-            List<Transaction> transactions = new List<Transaction>();   
-            return transactions;
+            //List<Transaction> transactions = new List<Transaction>();   
+            return list;
         }
     }
 }
