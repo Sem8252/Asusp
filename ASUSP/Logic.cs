7e7b19edebafdeb139da91448fb5d8cec7ba676c
Semyon Arkhipov <sem8252@yandex.ru>
2018-05-11 21:10:10 +0300

    Added file

---
+++ ASUSP/Logic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASUSP
+{
+    class Logic
+    {
+        public static Transaction AddFromScreen(string StartDate, string ExpDate, int Type, string Code,
+            string Country, string City, string Firm, int CellType)
+        {
+            Transaction newTransaction = new Transaction();
+            newTransaction.startDate = Convert.ToDateTime(StartDate);
+            newTransaction.expiryDate = Convert.ToDateTime(ExpDate);
+            newTransaction.productType = Type;
+            newTransaction.productCode = Convert.ToInt32(Code);
+            newTransaction.country = Country;
+            newTransaction.city = City;
+            newTransaction.firm = Firm;
+            newTransaction.cell = new Cell(Type, 1, "Test"); //ИСПРАВИТЬ
+            return newTransaction;
+        }
+
+        public static List<Transaction> Data { get; set; }
+        public static int StartLine { get; set; }
+    }
+}
